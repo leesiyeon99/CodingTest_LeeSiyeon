7cbfcbe0a20988da0fc8ea763866823e19aed228
leesiyeon99 <qsqs9090@naver.com>
2024-08-06 17:58:57 +0900

    [OtherSolution] Problem9

--- Problem9/Program.cs
+++ Problem9/Program.cs
@@ -37,6 +37,19 @@
                 }
                 return list.ToArray();
             }
+
+            public int[] OtherSolution1(int n)
+            {
+                List<int> answer = new List<int>();
+
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 1)
+                        answer.Add(i);
+                }
+
+                return answer.ToArray();
+            }
         }
     }
 }
